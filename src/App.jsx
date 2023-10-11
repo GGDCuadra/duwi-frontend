46bd20b7e54946f490c2cd6d76bfce3fa2a0da3f
GGDCuadra <133067189+GGDCuadra@users.noreply.github.com>
2023-10-11 15:58:50 -0300

    app.jsx modificado(rutas base creadas)

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,31 @@
 import { useState } from 'react'
+import {Route, Routes} from 'react-router-dom'
+import LandingPage from './components/LandingPage/LandingPage'
+import Home from './components/Home/Home'
+import Login from './components/Login/Login'
+import About from './components/AboutUs/AboutUs'
+import Register from './components/Register/Register'
 import './App.css'
+import MovieDetail from './components/MovieDetail/MovieDetail'
+import SeriesDetail from './components/SeriesDetail/SeriesDetail'
+import UserDashboard from './components/UserDashboard/UserDashboard'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-   <router>
-    //*aquí las rutas
-   </router>
+   <div>
+    <Routes>
+      <Route path='/' element={<LandingPage/>} />
+      <Route path='/home' element={<Home/>}/>
+      <Route path='/login' element={<Login/>}/>
+      <Route path='/register' element={<Register/>}/>
+      <Route path='/about' element={<About/>}/>
+      <Route path='/movie/:id' element={<MovieDetail/>}/>
+      <Route path='/serie/:id' element={<SeriesDetail/>}/>
+      <Route path='/user' element={<UserDashboard/>}/>
+    </Routes>
+   </div>
   )
 }
 
