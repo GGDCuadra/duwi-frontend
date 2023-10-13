208f707b9ef82d7d3cfca0a4d6e00d7f0b8e7274
GGDCuadra <133067189+GGDCuadra@users.noreply.github.com>
2023-10-13 10:49:43 -0300

    prueba

--- src/components/LandingPage/LandingPage.jsx
+++ src/components/LandingPage/LandingPage.jsx
@@ -1,9 +1,8 @@
 import React from 'react'
+import Prueba from '../buttonDePrueba/Prueba'
 //comentario que uso para ver si sí se sube mi repo :'c jaja borralo después que lo veas xD
 function LandingPage() {
   return (
-    <div>LandingPage</div>
+    <div>Landingpage</div>
   )
 }
-
-export default LandingPage
