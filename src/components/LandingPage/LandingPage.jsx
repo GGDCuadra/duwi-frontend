74f6365c9f573ff01b020faa2037acba546673b9
Wendy Calderón Rius <riuswendy@gmail.com>
2023-10-12 22:29:47 -0600

    landing, navbar y searchbar con estilos tailwind

--- src/components/LandingPage/LandingPage.jsx
+++ src/components/LandingPage/LandingPage.jsx
@@ -1,9 +1,22 @@
 import React from 'react'
-//comentario que uso para ver si sí se sube mi repo :'c jaja borralo después que lo veas xD
+import landing from './../../assets/landing.jpeg'
+
 function LandingPage() {
   return (
-    <div>LandingPage</div>
-  )
+    <div className="relative h-screen">
+      <img
+        src={landing}
+        alt="Background"
+        className="absolute inset-0 object-cover w-full h-full"
+        style={{ filter: 'blur(5px) brightness(0.3)' }}
+      />
+      <div className="absolute inset-0 flex flex-col justify-center items-center">
+        <h1 className="text-lila text-8xl font-extrabold mb-10">¿Buscas algo que ver?</h1>
+        <h2 className="text-clarito text-3xl font-normal mb-2">Encuentra tus películas y series favoritas, lleva un registro</h2>
+        <h2 className="text-clarito text-3xl font-normal">de cuáles has visto y no pierdas de vista los estrenos.</h2>
+      </div>
+    </div>
+  );
 }
 
 export default LandingPage
