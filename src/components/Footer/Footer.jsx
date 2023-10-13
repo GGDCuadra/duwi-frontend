44a65c98b531db214c18c40262e6faa0cd5f5245
Andrés Anguiano Delgado <111472374+Anngues@users.noreply.github.com>
2023-10-13 10:41:05 -0600

    Footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,9 +1,40 @@
-import React from 'react'
-//comentario que uso para ver si sí se sube mi repo :'c jaja borralo después que lo veas xD
+import React from 'react';
+import { Link } from 'react-router-dom';
+import logo from '../../assets/logoduwi.png';
 function Footer() {
   return (
-    <div>Footer</div>
-  )
+<div className="bg-gray-800 text-gray-300 py-8">
+  <div className="container mx-auto flex flex-wrap justify-between items-center">
+    <div className="w-full md:w-auto md:flex-grow-0 flex-shrink-0 mb-4 md:mb-0">
+      <img src={logo} alt="Logo" className="w-50 h-12 mt-2 ml-2" />
+    </div>
+    <div className="w-full md:w-auto md:flex-grow-0 flex-shrink-0">
+      <ul className="flex flex-wrap justify-center space-x-4">
+        <li>
+          <Link to="/donaciones" className="hover:text-moradito font-poppins">
+            Donaciones
+          </Link>
+        </li>
+        <li>
+          <Link to="/buzon-de-sugerencias" className="hover:text-moradito font-poppins">
+            Buzón de sugerencias
+          </Link>
+        </li>
+        <li>
+          <Link to="/acerca-de-nosotros" className="hover:text-moradito font-poppins">
+            Acerca de nosotros
+          </Link>
+        </li>
+      </ul>
+    </div>
+    <div className="w-full md:w-auto md:flex-grow-0 flex-shrink-0">
+      <p className="text-center text-sm font-poppins">
+        2023 Did U Watch It? - Todo el contenido externo pertenece a su dueño legal en cuanto a derechos.
+      </p>
+    </div>
+  </div>
+</div>
+);
 }
 
-export default Footer
+export default Footer;
--- src/components/LandingPage/LandingPage.jsx
+++ src/components/LandingPage/LandingPage.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
 import landing from './../../assets/landing.jpeg'
+import Footer from '../Footer/Footer';
 
 function LandingPage() {
   return (
-    <div className="relative h-screen">
+    <div className="flex flex-col min-h-screen">
+      <div className='flex-1 relative'>
       <img
         src={landing}
         alt="Background"
@@ -11,10 +13,12 @@ function LandingPage() {
         style={{ filter: 'blur(5px) brightness(0.3)' }}
       />
       <div className="absolute inset-0 flex flex-col justify-center items-center">
-        <h1 className="text-lila text-8xl font-extrabold mb-10">¿Buscas algo que ver?</h1>
-        <h2 className="text-clarito text-3xl font-normal mb-2">Encuentra tus películas y series favoritas, lleva un registro</h2>
-        <h2 className="text-clarito text-3xl font-normal">de cuáles has visto y no pierdas de vista los estrenos.</h2>
+        <h1 className="text-lila text-8xl font-black mb-10 font-poppins">¿Buscas algo que ver?</h1>
+        <h2 className="text-clarito text-3xl font-normal mb-2 font-poppins">Encuentra tus películas y series favoritas, lleva un registro</h2>
+        <h2 className="text-clarito text-3xl font-normal font-poppins">de cuáles has visto y no pierdas de vista los estrenos.</h2>
       </div>
+      </div>
+      <Footer />
     </div>
   );
 }
