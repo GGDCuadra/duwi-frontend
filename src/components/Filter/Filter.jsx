0aa1d9ec5a5083b692832b7111bb9d18773c7df6
GGDCuadra <133067189+GGDCuadra@users.noreply.github.com>
2023-10-16 14:04:15 -0300

    todo funciona

--- src/components/Filter/Filter.jsx
+++ src/components/Filter/Filter.jsx
@@ -2,7 +2,7 @@ import { useState } from "react";
 import style from './Filter.module.css'
 import { useDispatch } from "react-redux";
 import { getFilteredMovies } from "../../redux/actions";
-const Filters = ({ onFilterChange, currentFilters, currentMoviesPage,currentSeriesPage }) => {
+const Filters = ({type, onFilterChange, currentFilters, currentMoviesPage, currentSeriesPage }) => {
     const dispatch = useDispatch()
     const [filters, setFilters] = useState({
         genre: "",
@@ -29,23 +29,49 @@ const Filters = ({ onFilterChange, currentFilters, currentMoviesPage,currentSeri
             <form className={style.formFilters} onSubmit={handleSubmit}>
                 <div className={style.selectContainer}>
                     <select name="genre" onChange={handleChange}>
-                        <option value="">Seleciona un genero</option>
-                        <option value="Action">Acción</option>
-                        <option value="Adventure">Aventura</option>
-                        <option value="Drama">Drama</option>
-                        <option value="War">Guerra</option>
-                        <option value="Nature">Naturaleza</option>
-                        <option value="Crime">Crimen</option>
-                        <option value="Thriller">Thriller</option>
-                        <option value="Science-Fiction">Ciencia ficción</option>
-                        <option value="History">Historia</option>
-                        <option value="Biography">Biografia</option>
-                        <option value="Fantasy">Fantacia</option>
-                        <option value="Animation">Animación</option>
-                        <option value="Music">Musica</option>
-                        <option value="Romance">Romance</option>
-                        <option value="Mystery">Misterio</option>
-
+                    <option value="">Seleciona un genero</option>
+                        {type === 'movies' && (
+                            <>
+                                <option value="Action">Acción</option>
+                                <option value="Adventure">Aventura</option>
+                                <option value="Drama">Drama</option>
+                                <option value="War">Guerra</option>
+                                <option value="Crime">Crimen</option>
+                                <option value="Thriller">Thriller</option>
+                                <option value="Sci-Fi">Ciencia ficción</option>
+                                <option value="History">Historia</option>
+                                <option value="Biography">Biografia</option>
+                                <option value="Fantasy">Fantacia</option>
+                                <option value="Animation">Animación</option>
+                                <option value="Music">Musica</option>
+                                <option value="Romance">Romance</option>
+                                <option value="Mystery">Misterio</option>
+                            </>
+                        )}
+                        {type === 'series' && (
+                            <>
+                                <option value="Drama">Drama</option>
+                                <option value="Action">Acción</option>
+                                <option value="War">Guerra</option>
+                                <option value="Crime">Crimen</option>
+                                <option value="Thriller">Thriller</option>
+                                <option value="Nature">Naturaleza</option>
+                                <option value="Adventure">Aventura</option>
+                                <option value="Science-Fiction">Ciencia ficción</option>
+                                <option value="Western">Western</option>
+                                <option value="Mystery">Misterio</option>
+                                <option value="Supernatural">Supernatural</option>
+                                <option value="Family">Family</option>
+                                <option value="Romance">Romance</option>
+                                <option value="Comedy">Comedy</option>
+                                <option value="Fantasy">Fantasy</option>
+                                <option value="Medical">Medical</option>
+                                <option value="Anime">Anime</option>
+                                <option value="Food">Food</option>
+                                <option value="Travel">Travel</option>
+                                <option value="History">History</option>
+                            </>
+                        )}
                     </select>
                 </div>
                 <div className={style.selectContainer}>
