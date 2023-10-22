c46765ef2337c9e03606284cc2fc6ff17290036c
GGDCuadra <133067189+GGDCuadra@users.noreply.github.com>
2023-10-21 21:27:40 -0300

    seg

--- src/App.jsx
+++ src/App.jsx
@@ -11,6 +11,7 @@ import SeriesDetail from './components/SeriesDetail/SeriesDetail'
 import UserDashboard from './components/UserDashboard/UserDashboard'
 import FormCreate from './components/FormCreate/FormCreate'
 import Cards from './components/Cards/Cards'
+import Donation from './components/Donation/Donation'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -31,6 +32,7 @@ function App() {
       <Route path='/user' element={<UserDashboard/>}/>
       <Route path='/formCreateEdit/:type/:id' element={<FormCreate/>}/>
       <Route path='/formCreateEdit/' element={<FormCreate/>}/>
+      <Route path='/donaciones' element={<Donation/>} />
     </Routes>
    </div>
   )
--- src/main.jsx
+++ src/main.jsx
@@ -1,20 +1,23 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
+import { PayPalScriptProvider } from '@paypal/react-paypal-js'
 import App from './App.jsx';
 import store from './redux/store.jsx'
 import { Provider } from 'react-redux';
-
 const root = document.getElementById('root');
-
 const rootElement = createRoot(root);
+
+const { VITE_PAYPAL_CLIENT_ID } = import.meta.env
 
 rootElement.render(
   <React.StrictMode>
-    <Provider store={store}>
+    <PayPalScriptProvider options={{ clientId: VITE_PAYPAL_CLIENT_ID }}>
+      <Provider store={store}>
         <BrowserRouter>
-      <App />
-      </BrowserRouter>
-    </Provider>
+          <App />
+        </BrowserRouter>
+      </Provider>
+    </PayPalScriptProvider>
   </React.StrictMode>
 );
