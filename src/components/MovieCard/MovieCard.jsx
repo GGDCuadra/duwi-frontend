450eab9d7994fb8c235903a9e78a12ce8287023d
Dilan01188 <130785250+Dilan01188@users.noreply.github.com>
2023-10-24 10:43:23 -0500

    funciones de dashbord danna no dañar

--- src/components/MovieCard/MovieCard.jsx
+++ src/components/MovieCard/MovieCard.jsx
@@ -1,7 +1,7 @@
 import { Link } from "react-router-dom";
 function MovieCard({ _id, Poster_Link, Series_Title, Genre, Released_Year }) {
   return (
-    <div className="relative w-60 h-80 rounded-lg shadow-md overflow-hidden group">
+    <div className="relative w-60 h-80 rounded-3xl shadow-md overflow-hidden group">
       <Link to={`/movie/${_id}`}>
       <div className="w-full rounded-lg transition-filter group-hover:blur-[2px] ">
         <img
@@ -9,7 +9,7 @@ function MovieCard({ _id, Poster_Link, Series_Title, Genre, Released_Year }) {
           alt={Series_Title}
         />
       </div>
-      <div className="absolute inset-0 opacity-0 group-hover:opacity-100 transition-opacity duration-300 ease-in-out flex flex-col justify-end items-center text-center group-hover:text-xl">
+      <div className="absolute inset-0 bg-black opacity-0 group-hover:opacity-80 transition-opacity duration-300 ease-in-out flex flex-col justify-end items-center text-center group-hover:text-xl">
         <h2 className="text-lila font-bold font-poppins text-base p-5">
           {Series_Title}
         </h2>
--- src/components/SeriesCard/SeriesCard.jsx
+++ src/components/SeriesCard/SeriesCard.jsx
@@ -3,12 +3,12 @@ import { Link } from 'react-router-dom'
 
 function SeriesCard({name, _id, genres, image, premiered}) {
   return (
-    <div className="relative w-60 h-80 rounded-lg shadow-md overflow-hidden group">
+    <div className="relative w-60 h-80 rounded-3xl shadow-md overflow-hidden group">
       <Link to={`/serie/${_id}`}>
       <div className="w-full rounded-lg transition-filter group-hover:blur-[2px] ">
         <img src={image} />
       </div>
-      <div className="absolute inset-0 opacity-0 group-hover:opacity-100 transition-opacity duration-300 ease-in-out flex flex-col justify-end items-center text-center">
+      <div className="absolute inset-0 bg-black opacity-0 group-hover:opacity-80 transition-opacity duration-300 ease-in-out flex flex-col justify-end items-center text-center">
         <h2 className="text-lila font-bold font-poppins text-base p-5">{name}</h2>
         <h2 className="text-lila font-bold font-poppins text-base p-5">{genres}</h2>
         <h2 className="text-lila font-bold font-poppins text-base p-5">{premiered}</h2>
