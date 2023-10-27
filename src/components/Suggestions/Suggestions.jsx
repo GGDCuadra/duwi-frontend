79eeccc36de440d930331a7779dc5f895fe65ec9
LDZamblera <lucaszamblers@gmail.com>
2023-10-26 22:46:42 -0300

    act sugerencias y about

--- src/components/Suggestions/Suggestions.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-//comentario que uso para ver si sí se sube mi repo :'c jaja borralo después que lo veas xD
-function Suggestions() {
-  return (
-    <div>Suggestions</div>
-  )
-}
-
-export default Suggestions
