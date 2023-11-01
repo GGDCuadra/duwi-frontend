a49dea11c3f9da122c9911e3708b7b34bad2b856
Dilan01188 <130785250+Dilan01188@users.noreply.github.com>
2023-11-01 15:23:27 -0500

    ojala funcione

--- src/main.jsx
+++ src/main.jsx
@@ -5,23 +5,23 @@ import { PayPalScriptProvider } from '@paypal/react-paypal-js'
 import App from './App.jsx';
 import store from './redux/store.jsx';
 import { Provider } from 'react-redux';
-import { Auth0Provider } from '@auth0/auth0-react'; // Importa Auth0Provider
-
-
+import { Auth0Provider } from '@auth0/auth0-react'; 
 
 const root = document.getElementById('root');
 const rootElement = createRoot(root);
 
 const { VITE_PAYPAL_CLIENT_ID } = import.meta.env
+const {VITE_APP_AUTH0_DOMAIN, VITE_APP_AUTH0_CLIENT_ID}  = import.meta.env
+
 
 rootElement.render(
   <React.StrictMode>
     <PayPalScriptProvider options={{ clientId: "ATMLVmLHv0YEwCBlrsTN876rIIPsFqEHl6nuZyidnWBeWL3ongFuZb2iIS6pw9Q-t0mha_tcypiqG_jb" }}>
       <Provider store={store}>
         <BrowserRouter>
-          <Auth0Provider
-            domain="dev-yan4u8u82wxf2qfz.us.auth0.com"
-            clientId="3tvQ9nFAkeQ6gs6UzoAN19PHHQTEl9N3"
+        <Auth0Provider
+            domain={VITE_APP_AUTH0_DOMAIN}
+            clientId={VITE_APP_AUTH0_CLIENT_ID}
             redirectUri={window.location.origin + '/dashboard'}
           >
             <App />
